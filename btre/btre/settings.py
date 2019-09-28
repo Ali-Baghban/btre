03b8d51759a4448edd82d23ad506d905b1fbbd56
Ali Baghban <33204546+Ali-Baghban@users.noreply.github.com>
2019-09-28 23:15:06 +0330

    Removed secret key from settings.py

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+=mimg7x8ihp0qus_8fhwnood(y907hsz&(x6c=9_c&enre-la'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
